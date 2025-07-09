dc0e76acf51b2a50b5bca18edc3bc0876387342d
basamaryan <basam.aryan@gmail.com>
2025-07-09 09:01:58 -0400

    sm8150-common: Update WFD system blobs from dada OS2.0.217.0.WOCMIXM
    Change-Id: I58ede53f3f6e8ec88f8aaecca453c985d792bff6
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- sm8150-common-vendor.mk
+++ sm8150-common-vendor.mk
@@ -514,6 +514,7 @@ PRODUCT_PACKAGES += \
     libwfduibcsrc \
     libwfduibcsrcinterface \
     vendor.qti.hardware.wifidisplaysession@1.0 \
+    vendor.qti.hardware.wifidisplaysession_aidl-V1-ndk \
     com.qualcomm.qti.dpm.api@1.0 \
     lib-imsvideocodec \
     lib-imsvt \
@@ -614,8 +615,7 @@ PRODUCT_PACKAGES += \
     xtwifi-client \
     xtwifi-inet-agent \
     dpmd \
-    wfdservice \
-    wfdservice64
+    wfdservice
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
